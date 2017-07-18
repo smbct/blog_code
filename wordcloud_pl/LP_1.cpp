9e3a8605c8ef3cc9fd306beece2bba091673e910
smbct <buchet.s@laposte.net>
2017-07-18 20:51:37 +0200

    ne marche pas

--- wordcloud_pl/LP_1.cpp
+++ wordcloud_pl/LP_1.cpp
@@ -20,9 +20,15 @@ void LP_1::calculer(vector<Rect*>& _listeRect) {
 
     bool continuer = true;
 
-    for(int i = 0; i < _listeRect.size(); i++) {
+    _listeEssai.push_back(_listeRect.at(0));
+    for(int i = 1; i < _listeRect.size(); i++) {
 
         _listeEssai.push_back(_listeRect.at(i));
+
+        cout << "testA" << endl;
+        cout << _listeEssai.size() << endl;
+        cout << i << endl;
+        cout << endl << endl;
 
         if(!resoudre(_listeEssai)) { // si le dernier ajouté ne passe pas, il est retiré
             _listeEssai.pop_back();
@@ -30,12 +36,16 @@ void LP_1::calculer(vector<Rect*>& _listeRect) {
             _listeEssai.back()->utilise = true;
         }
 
+        cout << "testB" << endl;
+
     }
 
 }
 
 /*----------------------------------------------------------------------------*/
 bool LP_1::resoudre(list<Rect*>& _listeEssai) {
+
+    bool res = true;
 
     // Create a problem pointer.  We use the base class here.
     OsiSolverInterface *si;
@@ -45,13 +55,14 @@ bool LP_1::resoudre(list<Rect*>& _listeEssai) {
 
     // nombre de variables : 2 par textes (position x et position y)
     int n_cols = _listeEssai.size()*2;
+    n_cols += (_listeEssai.size()*(_listeEssai.size()-1))*2;
+
     double * objective    = new double[n_cols];//the objective coefficients
     double * col_lb       = new double[n_cols];//the column lower bounds
     double * col_ub       = new double[n_cols];//the column upper bounds
 
     int i = 0;
     for(Rect* rect : _listeEssai) {
-
         // Define the objective coefficients.
         objective[i*2] = 0.;
         objective[i*2+1] = 0.;
@@ -62,11 +73,22 @@ bool LP_1::resoudre(list<Rect*>& _listeEssai) {
         col_lb[i*1+1] = 0.0;
         col_ub[i*2] = 800-rect->larg;
         col_ub[i*2+1] = 600-rect->haut;
-
+        i ++;
     }
 
+    // variables binaires
+    for(i = _listeEssai.size()*2; i < n_cols; i++) {
+        si->setInteger(i);
+        col_lb[i] = 0;
+        col_ub[i] = 1;
+        objective[i] = 0;
+    }
 
-    int n_rows = 2;
+    // contraintes: autant que de variables binaires*2
+    int n_rows = (_listeEssai.size()*(_listeEssai.size()-1))*4;
+
+    double M = 800; // constante assez grande
+
     double * row_lb = new double[n_rows]; //the row lower bounds
     double * row_ub = new double[n_rows]; //the row upper bounds
 
@@ -74,21 +96,80 @@ bool LP_1::resoudre(list<Rect*>& _listeEssai) {
     CoinPackedMatrix * matrix =  new CoinPackedMatrix(false,0,0);
     matrix->setDimensions(0, n_cols);
 
+    auto it = _listeEssai.begin();
+    it ++;
+    int ind1 = 1;
+    int indCt = 0;
+
+
+    while(it != _listeEssai.end()) {
+
+        int ind2 = 0;
+        for(auto it2 = _listeEssai.begin(); it2 != it; it2++) {
+
+            CoinPackedVector row1;
+            CoinPackedVector row2;
+            CoinPackedVector row3;
+            CoinPackedVector row4;
+
+            // contrainte en x côté gauche
+            row1.insert(2*ind2, -1.);
+            row1.insert(2*ind1, 1.);
+            row1.insert(_listeEssai.size()*2-1+ind1, M);
+            row_lb[indCt] = (*it)->larg;
+            row_ub[indCt] = si->getInfinity();
+            matrix->appendRow(row1);
+            indCt ++;
+
+            // contrainte en x côté droit
+            row2.insert(2*ind2, -1.);
+            row2.insert(2*ind1, 1.);
+            row2.insert(_listeEssai.size()*2-1+ind1, -1.0*M);
+            row_lb[indCt] = -1.0 * si->getInfinity();
+            row_ub[indCt] = M - (*it2)->larg;
+            matrix->appendRow(row2);
+            indCt ++;
+
+            // contrainte en y en haut
+            row3.insert(2*ind2+1, -1.);
+            row3.insert(2*ind1+1, 1.);
+            row3.insert(_listeEssai.size()*2-1+ind1+1, M);
+            row_lb[indCt] = (*it)->haut;
+            row_ub[indCt] = si->getInfinity();
+            matrix->appendRow(row3);
+            indCt ++;
+
+            // contrainte en y en bas
+            row4.insert(2*ind2+1, -1.);
+            row4.insert(2*ind1+1, 1.);
+            row4.insert(_listeEssai.size()*2-1+ind1+1, -1.0*M);
+            row_lb[indCt] = -1.0 * si->getInfinity();
+            row_ub[indCt] = M - (*it2)->haut;
+            matrix->appendRow(row4);
+            indCt ++;
+
+            ind2 ++;
+        }
+
+        ind1 ++;
+        it ++;
+    }
+
     //1 x0 + 2 x1 <= 3  =>  -infinity <= 1 x0 + 2 x2 <= 3
-    CoinPackedVector row1;
-    row1.insert(0, 1.0);
-    row1.insert(1, 2.0);
-    row_lb[0] = -1.0 * si->getInfinity();
-    row_ub[0] = 3.0;
-    matrix->appendRow(row1);
+    // CoinPackedVector row1;
+    // row1.insert(0, 1.0);
+    // row1.insert(1, 2.0);
+    // row_lb[0] = -1.0 * si->getInfinity();
+    // row_ub[0] = 3.0;
+    // matrix->appendRow(row1);
 
     //2 x0 + 1 x1 <= 3  =>  -infinity <= 2 x0 + 1 x1 <= 3
-    CoinPackedVector row2;
-    row2.insert(0, 2.0);
-    row2.insert(1, 1.0);
-    row_lb[1] = -1.0 * si->getInfinity();
-    row_ub[1] = 3.0;
-    matrix->appendRow(row2);
+    // CoinPackedVector row2;
+    // row2.insert(0, 2.0);
+    // row2.insert(1, 1.0);
+    // row_lb[1] = -1.0 * si->getInfinity();
+    // row_ub[1] = 3.0;
+    // matrix->appendRow(row2);
 
     //load the problem to OSI
     si->loadProblem(*matrix, col_lb, col_ub, objective, row_lb, row_ub);
@@ -99,6 +180,8 @@ bool LP_1::resoudre(list<Rect*>& _listeEssai) {
     // Solve the (relaxation of the) problem
     si->initialSolve();
 
+    si->branchAndBound();
+
     // Check the solution
     if ( si->isProvenOptimal() ) {
         std::cout << "Found optimal solution!" << std::endl;
@@ -107,13 +190,22 @@ bool LP_1::resoudre(list<Rect*>& _listeEssai) {
         const double *solution;
         solution = si->getColSolution();
         // We could then print the solution or examine it.
+        res = true;
     }
     else {
         std::cout << "Didn’t find optimal solution." << std::endl;
         // Could then check other status functions.
+        res = false;
     }
 
-    return true;
+    delete[] objective;
+    delete[] col_lb;
+    delete[] col_ub;
+    delete[] row_lb;
+    delete[] row_ub;
+    delete si;
+
+    return res;
 }
 
 void test() {
